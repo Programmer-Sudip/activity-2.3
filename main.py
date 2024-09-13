e9b57aff07ceec90696fe039f36bc5b842f6ce24
Programmer Sudip <sudipmicrosoft259@gmail.com>
2024-09-13 22:24:45 +0600

    list

---
+++ main.py
@@ -0,0 +1,23 @@
+class student:
+    grade = 11
+    name = "Penguin"
+
+    def introduction(self):
+        print("Hi I am a student")
+
+
+    def details(self):    
+        print("My name is ", self.name)
+        print("I am study in grade",self.grade)
+
+
+
+ob = student()
+ob.introduction()
+ob.details()
+
+
+
+
+
+
